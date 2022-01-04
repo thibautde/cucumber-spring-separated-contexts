25fe24bbd7df38d6b7790b94d6dc959abaa394ec
Thibaut Debruille <t@example.com>
2022-01-04 13:38:48 +0100

    Use JUnit Platform

--- src/test/java/separated_contexts/features/printing/RunPrintingCucumberTests.java
+++ src/test/java/separated_contexts/features/printing/RunPrintingCucumberTests.java
@@ -1,15 +1,21 @@
 package separated_contexts.features.printing;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    glue = "separated_contexts.features.printing",
-    features = "classpath:separated_contexts/features/printing")
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(
+    key = GLUE_PROPERTY_NAME,
+    value = "separated_contexts.features.printing")
+@SelectClasspathResource("separated_contexts/features/printing")
 @CucumberContextConfiguration
 @SpringBootTest(classes = PrintingSpringTestConfig.class)
 public class RunPrintingCucumberTests {
--- src/test/java/separated_contexts/features/reporting/RunReportCucumberTests.java
+++ src/test/java/separated_contexts/features/reporting/RunReportCucumberTests.java
@@ -1,15 +1,21 @@
 package separated_contexts.features.reporting;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    glue = "separated_contexts.features.reporting",
-    features = "classpath:separated_contexts/features/reporting")
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(
+    key = GLUE_PROPERTY_NAME,
+    value = "separated_contexts.features.reporting")
+@SelectClasspathResource("separated_contexts/features/reporting")
 @CucumberContextConfiguration
 @SpringBootTest(classes = ReportingSpringTestConfig.class)
 public class RunReportCucumberTests {
